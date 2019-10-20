8a7d714556d753f83dcaa1f79642955a525fea1b
zhangpanpan <zjrzpp0414@163.com>
2019-10-20 14:17:49 +0800

    first commit

---
+++ webhook.js
@@ -0,0 +1,13 @@
+var http = require('http')
+let server = http.createServer(function (req, res) {
+  console.log(req.method, req.url)
+  if(req.method == 'POST' && req.url === '/webhook') {
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ok: true}));
+  } else {
+    res.end('Not Found');
+  }
+})
+server.listen(4000, function() {
+  console.log('webHook服务已经在4000端口上启用了')
+})
