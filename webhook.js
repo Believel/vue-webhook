b6b4673ea69d8e7aca0b8db416302b7296e22cb3
zhangpanpan <zjrzpp0414@163.com>
2019-10-20 16:03:44 +0800

    modify

--- webhook.js
+++ webhook.js
@@ -20,29 +20,31 @@ let server = http.createServer(function (req, res) {
       let signature = req.headers['x-hub-signature']
       if(signature !=sign(body)) {
         return res.end('Not Allowed')
+      } else {
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify({ok: true}));
+        if (event === 'push') { // 开始部署
+          let payload = JSON.parse(body)
+          let child = spawn('sh',[`./${payload.repository.name}.sh`]);
+          let buffers = [];
+          child.stdout.on('data',function(buffer){
+              buffers.push(buffer);
+          });
+            child.stdout.on('end',function(buffer){
+              let logs = Buffer.concat(buffers).toString();
+              console.log(logs)
+              // sendMail(`
+              //     <h1>部署日期: ${new Date()}</h1>
+              //     <h2>部署人: ${payload.pusher.name}</h2>
+              //     <h2>部署邮箱: ${payload.pusher.email}</h2>
+              //     <h2>提交信息: ${payload.head_commit&&payload.head_commit['message']}</h2>
+              //     <h2>布署日志: ${logs.replace("\r\n",'<br/>')}</h2>
+              // `);
+          });
+        }
       }
     })
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ok: true}));
-    if (event === 'push') { // 开始部署
-      let payload = JSON.parse(body)
-      let child = spawn('sh',[`./${payload.repository.name}.sh`]);
-      let buffers = [];
-      child.stdout.on('data',function(buffer){
-          buffers.push(buffer);
-      });
-        child.stdout.on('end',function(buffer){
-          let logs = Buffer.concat(buffers).toString();
-          console.log(logs)
-          // sendMail(`
-          //     <h1>部署日期: ${new Date()}</h1>
-          //     <h2>部署人: ${payload.pusher.name}</h2>
-          //     <h2>部署邮箱: ${payload.pusher.email}</h2>
-          //     <h2>提交信息: ${payload.head_commit&&payload.head_commit['message']}</h2>
-          //     <h2>布署日志: ${logs.replace("\r\n",'<br/>')}</h2>
-          // `);
-      });
-    }
+    
   } else {
     res.end('Not Found');
   }
